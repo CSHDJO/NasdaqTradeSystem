df8ff69a6be117dc67dd5243fc995af04214888c
DLEE19637 <d.lee@chipsoft.com>
2025-07-09 10:34:07 +0200

    init

---
+++ Bots/DeonBot/DeonBot.cs
@@ -0,0 +1,19 @@
+﻿using NasdaqTrader.Bot.Core;
+
+namespace ExampleTraderBot;
+
+public class DeonBot : ITraderBot
+{
+    public string CompanyName => "BankruptSolutions";
+
+    public async Task DoTurn(ITraderSystemContext systemContext)
+    {
+        var listings = systemContext.GetListings();
+
+        for(int i = 0; i < 4; i++)
+        {
+            Random random = new Random();
+            systemContext.BuyStock(this, listings[random.Next(0, listings.Count)], random.Next(1, 10));
+        }
+    }
+}
