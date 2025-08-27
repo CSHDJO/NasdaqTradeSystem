c94c680e0b234415eb79f524e1b3667ba3718dc3
Lance-88 <129681882+Lance-88@users.noreply.github.com>
2025-08-27 09:01:57 +0200

    Nieuwe bot die wél iets kan

---
+++ Bots/AleixBot.Tests/ListingPickerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace AleixBot.Tests
+{
+    public class Tests
+    {
+        private ListingPicker _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        //[Test]
+        //public void ListingPicker_CalculateBestListingForDate(DateOnly date)
+        //{
+        //    Assert.Pass();
+        //}
+
+
+        //Initializes
+        //Returns best listing available for a specific date
+
+    }
+}
